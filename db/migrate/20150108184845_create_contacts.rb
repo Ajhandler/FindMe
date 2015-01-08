b1d4511a1db2ee2f5fdd21638323c486f2144e50
Alexander Handler <Alexjhandler@gmail.com>
2015-01-08 13:53:37 -0500

    associate contacts and user model

--- db/migrate/20150108184845_create_contacts.rb
+++ db/migrate/20150108184845_create_contacts.rb
@@ -3,7 +3,7 @@ class CreateContacts < ActiveRecord::Migration
     create_table :contacts do |t|
       t.string :name
       t.string :phone_number
-
+      t.integer :user_id
       t.timestamps null: false
     end
   end
